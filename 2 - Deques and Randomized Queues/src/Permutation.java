6e7e354c96733208551df29bd92aab233745b508
Maxim Vasilic <lmantenl@gmail.com>
2020-08-13 18:40:25 +0300

    Initial commit

--- 2 - Deques and Randomized Queues/src/Permutation.java
+++ 2 - Deques and Randomized Queues/src/Permutation.java
@@ -3,22 +3,22 @@ import edu.princeton.cs.algs4.StdRandom;
 
 public class Permutation {
     public static void main(String[] args) {
-        RandomizedQueue<String> queue = new RandomizedQueue<>();
         int k = Integer.parseInt(args[0]);
-
         if (k == 0) return;
 
-        int i = 0;
-        for (; i < k; i++) queue.enqueue(StdIn.readString());
+        RandomizedQueue<String> queue = new RandomizedQueue<>();
+
+        int count = 0;
+        for (; count < k; count++) queue.enqueue(StdIn.readString());
+
         while (!StdIn.isEmpty()) {
             String in = StdIn.readString();
-            int index = StdRandom.uniform(0, i + 1);
+            int index = StdRandom.uniform(count + 1);
             if (index < k) {
                 queue.dequeue();
                 queue.enqueue(in);
             }
-
-            i++;
+            count++;
         }
 
         for (String s : queue) System.out.println(s);
